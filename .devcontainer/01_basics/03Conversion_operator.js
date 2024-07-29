44483fa52fc76b9ebab76e07dc7e560802475f9b
Sarthak Sharma <isarthak2005@gmail.com>
2024-07-29 16:30:18 +0000

    conversion of data type

---
+++ .devcontainer/01_basics/03Conversion_operator.js
@@ -0,0 +1,14 @@
+let score = 232
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0=> false
+// ""= false
+//"string" =>true
