1e4434b23212318c99549c639431ffb19b8a37be
Sarthak Sharma <isarthak2005@gmail.com>
2024-07-31 17:44:10 +0000

    string in js

--- .devcontainer/01_basics/05_string.js
+++ .devcontainer/01_basics/05_string.js
@@ -1,4 +1,17 @@
 const name = "sarthak"
 const repo=23
 
-console.log("hello my name is ${name} and my repo count is ${repo}");
+console.log(`hello my name is ${name} and my repo count is ${repo}`);
+
+
+const gameName = new String('SarthakSaru')
+console.log(gameName[0])
+console.log(gameName.__proto__)
+
+console.log(gameName.length)
+
+const anotherString = gameName.substring(2,6)
+console.log(anotherString)
+
+const url = "https://sarthak.com/Sarthak20Sharma%"
+console.log(url.replace('20%','-'))
